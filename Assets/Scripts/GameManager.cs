969eb5bc5295907807df0d0ade82b3b7bb90bec2
Doug Whitley <dwhitley@pdx.edu>
2017-07-09 15:25:37 -0700

    basic project structure setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Level Manager for the Seals of Fate Handles creating the level and
+// delegates the room creation 
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+	private LevelManager levelScript;
+
+
+	// Use this for initialization
+	void Awake () {
+
+		// Singleton
+		if(instance == null)
+			instance = this;
+		else if(instance != this)
+			Destroy(gameObject);
+		DontDestroyOnLoad(gameObject)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
