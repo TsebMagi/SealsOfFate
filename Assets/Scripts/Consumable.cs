3a9645762f3192d262ef826eb78e251411139cd2
mgieger <mrgieger@gmail.com>
2017-07-16 15:54:46 -0700

    Basic interface implemented and fish class created.

---
+++ Assets/Scripts/Consumable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Consumable : MonoBehaviour, IInteractable {
+
+//    public int HealthMod;
+ //   public int ManaMod;
+
+    public void Interact()
+    {
+        Consume();
+    }
+
+    virtual public void Consume()
+    {
+        // GameManager.instance.playerHealth += HealthMod;
+        //     GameManager.instance.playerMana += ManaMod;
+    }
+
+}
+/*
+public class Fish : Consumable {
+
+    public Fish()
+    {
+
+    }
+
+    public void Consume()
+    {
+
+    }
+
+}*/
---
+++ Assets/Scripts/Consumables/Fish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : Consumable {
+
+    public int Modifier;
+    public int HealthMod;
+
+    public Fish()
+    {
+        Modifier = 1;
+        HealthMod = 20;
+    }
+
+    public override void Consume()
+    {
+        GameManager.instance.playerHealth += HealthMod;
+    }
+
+}
