4225abddd7dd5bb27809a48efa9c4af3312f3453
mgieger <mgieger@pdx.edu>
2017-07-17 14:52:04 -0700

    Update and rename Fish.cs to Food.cs

--- Assets/Scripts/Consumables/Food.cs
+++ Assets/Scripts/Consumables/Food.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fish : Consumable {
+public class Food : Consumable {
 
-    public int Modifier;
+    public int Multiplier;
     public int HealthMod;
 
-    public Fish()
+    public Food()
     {
-        Modifier = 1;
+        Multiplier = 1;
         HealthMod = 5;
     }
 
@@ -18,7 +18,7 @@ public class Fish : Consumable {
         //Increases the player's health by 5 * the set modifier for the fish in the unity engine.
         //Set up this way for ease of generating different "food/fish" consumables that will provide
         //different amounts of health.
-        GameManager.instance.playerHealth += HealthMod * Modifier;
+        GameManager.instance.playerHealth += HealthMod * Multiplier;
     }
 
 }
