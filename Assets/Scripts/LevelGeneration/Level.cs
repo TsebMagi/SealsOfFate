52a218172e4e60646ccbeba7b3023baf56f3a178
Doug <TsebMagi@users.noreply.github.com>
2017-07-31 13:00:27 -0700

    Level gen (#84)
    * added folders to Prefab and updated the generation to allow prefabs as level options
    
    * random (poorly optimized) item placement
    
    * updated the decoration algorithm to believe in spreading the welth
    
    * Fix comparison to non-nullable type. (#85)
    
    * Jp cleanup code style (#88)
    
    * Indents parameters that are moved to a new line to improve readability
    
    * Finishes combat implementation (#86)
    
    * Finishes combat implementation
    
    - When an enemy's HP is depleted, it is removed from the board.
    - When the player's HP is depleted, it is removed from the board and an Exception is thrown.
    
    * Adds UnregisterEnemy in GameManager
    
    * Fixes several pathfinding bugs and adds logging (#89)
    
    * Fixes several pathfinding bugs and adds logging
    
    * Fixes #80
    * Fixes #81
    * Fixes a bug in BucketQueue where the iterator was not iterating
    through all buckets
    * Adds warning log messages in SearchAStar for unusual or undesirable
    circumstances
    * Refactors some vector.equals comparisons to == as per Unity doc
    reccomendation.
    
    * Tweaks a few parameters for better behavior and logging
    
    * Reduces the squared distance before giving up by pathfinding to 500
    * Changes the start point for debug lines drawn because of giving up.
    * Removes an old TODO that was accidently left in the code
    * Removes a couple of old asserts that didn't need to be in the code
    
    * Adds more pathfinding improvements and bugfixes
    
    * Fixes #82
    * Refactors getConnections to use raycasts instead of checking the
    tilemanager
    * Moves the distance check to the correct location
    * Changes the signature of SearchAStar constructor. Now takes an
    reference to the calling instance. Modifies Enemy.cs accordingly.
    
    * Cleans up some old extraneous code and comments.
    
    * Fixes a typo made in the merge conflict resolution
    
    * Formats GameManager.cs and Enemy.cs (#87)
    
    * Formats GameManager.cs
    
    - Resharper run through GameManager.cs
    - Logic clean up inside GameManager.Update to reduce nesting
    - Adds comments to Enemy.cs
    - Cleans up variable names in Enemy
    
    * Creates new internal property for CurrentLevelFeatureMap
    
    * Fixes IsInState
    
    * Implement requested changes
    
    - [ ] Cleaned up the logic of decoration
    - [ ] Added Consts for the magic numbers
    - [ ] Resharper Run
    - [ ] Excludes the spawn chunk from the decoration algorithm

--- Assets/Scripts/LevelGeneration/Level.cs
+++ Assets/Scripts/LevelGeneration/Level.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 namespace Assets.Scripts.LevelGeneration {
     public class Level : Feature {
+        private const int CullRatio = 2;
+        private const int CullThreshold = 6;
+        private const float EnemyDropOffset = 0.25f;
+        private const float ObstacleDropOffset = 0.25f;
+        private const float LootDropOffset = 0.25f;
+        private int _chunkSize;
         public Feature MiniMap = new Feature();
-        private const int CULL_RATIO = 2;
-        private const int CULL_THRESHOLD = 6;
-        private int chunkSize;
 
         /// <summary>
         ///     This Function generates a level and populates it,
@@ -17,9 +20,14 @@ namespace Assets.Scripts.LevelGeneration {
         /// <param name="enemyWeight">The maximum ratio of enemies for the level</param>
         /// <param name="lootWeight">The maximum ratio of loot for the level</param>
         /// <param name="obstaclesWeight">The maximum ratio of obstacles for the level</param>
-        public void Generate(int minChunks, int maxChunks, int chunkDimension, float enemyWeight, float lootWeight, float obstaclesWeight) {
+        public void Generate(int minChunks,
+                             int maxChunks,
+                             int chunkDimension,
+                             float enemyWeight,
+                             float lootWeight,
+                             float obstaclesWeight) {
             // setup the graph
-            chunkSize = chunkDimension;
+            _chunkSize = chunkDimension;
             var numChunks = Random.Range(minChunks, maxChunks);
             // size the map to allow for each room take up the maximum and leave space for tunnels
             XRange.max = YRange.max = chunkDimension * numChunks + 2;
@@ -29,7 +37,7 @@ namespace Assets.Scripts.LevelGeneration {
             BuildMiniMap(numChunks);
             BuildChunks(chunkDimension);
             FillInWalls();
-            Decorate(numChunks,enemyWeight,lootWeight,obstaclesWeight);
+            Decorate(numChunks, enemyWeight, lootWeight, obstaclesWeight);
         }
 
         /// <summary>
@@ -46,38 +54,41 @@ namespace Assets.Scripts.LevelGeneration {
             // place chunks until there are none left to place
             for (var i = 0; i < numChunks; ++i) {
                 // grab a random chunk from the list of potential slots if there are any.
-                if (potentialSlots.Count > 0) {
-                    var toPlace = potentialSlots[Random.Range(0, potentialSlots.Count)];
-                    // remove the chosen chunk from the future options
-                    potentialSlots.Remove(toPlace);
-                    // if the number of available spots is large cull some to produce more interesting options
-                    // Both the number that count is compared to and the ratio to cull shoould be played with as meta parameters
-                    if (potentialSlots.Count > CULL_THRESHOLD) {
-                        var numToRemove = potentialSlots.Count / CULL_RATIO;
-                        for (var j = 0; j < numToRemove - 1; ++j) {
-                            var toRemove = potentialSlots[Random.Range(0, potentialSlots.Count)];
-                            potentialSlots.Remove(toRemove);
-                        }
+                if (potentialSlots.Count <= 0) {
+                    continue;
+                }
+                var toPlace = potentialSlots[Random.Range(0, potentialSlots.Count)];
+                // remove the chosen chunk from the future options
+                potentialSlots.Remove(toPlace);
+                // if the number of available spots is large cull some to produce more interesting options
+                // Both the number that count is compared to and the ratio to cull shoould be played with as meta parameters
+                if (potentialSlots.Count > CullThreshold) {
+                    var numToRemove = potentialSlots.Count / CullRatio;
+                    for (var j = 0; j < numToRemove - 1; ++j) {
+                        var toRemove = potentialSlots[Random.Range(0, potentialSlots.Count)];
+                        potentialSlots.Remove(toRemove);
                     }
-                    // Mark the chosen spot 
-                    MiniMap.FeatureMap[(int) toPlace.x, (int) toPlace.y] = 1;
-                    // the following blocks check the cardinal directions and if they are in bounds and have not already on the list they are added as available spots.
-                    if (toPlace.x < MiniMap.FeatureMap.GetLength(0) - 1 && MiniMap.FeatureMap[(int) toPlace.x + 1, (int) toPlace.y] != 1 &&
-                        potentialSlots.Contains(new Vector2(toPlace.x + 1, toPlace.y)) != true) {
-                        potentialSlots.Add(new Vector2(toPlace.x + 1, toPlace.y));
-                    }
-                    if (toPlace.x > 0 && MiniMap.FeatureMap[(int) toPlace.x - 1, (int) toPlace.y] != 1 &&
-                        potentialSlots.Contains(new Vector2(toPlace.x - 1, toPlace.y)) != true) {
-                        potentialSlots.Add(new Vector2(toPlace.x - 1, toPlace.y));
-                    }
-                    if (toPlace.y < MiniMap.FeatureMap.GetLength(0) - 1 && MiniMap.FeatureMap[(int) toPlace.x, (int) toPlace.y + 1] != 1 &&
-                        potentialSlots.Contains(new Vector2(toPlace.x, toPlace.y + 1)) != true) {
-                        potentialSlots.Add(new Vector2(toPlace.x, toPlace.y + 1));
-                    }
-                    if (toPlace.y > 0 && MiniMap.FeatureMap[(int) toPlace.x, (int) toPlace.y - 1] != 1 &&
-                        potentialSlots.Contains(new Vector2(toPlace.x, toPlace.y - 1)) != true) {
-                        potentialSlots.Add(new Vector2(toPlace.x, toPlace.y - 1));
-                    }
+                }
+                // Mark the chosen spot 
+                MiniMap.FeatureMap[(int) toPlace.x, (int) toPlace.y] = 1;
+                // the following blocks check the cardinal directions and if they are in bounds and have not already on the list they are added as available spots.
+                if (toPlace.x < MiniMap.FeatureMap.GetLength(0) - 1 &&
+                    MiniMap.FeatureMap[(int) toPlace.x + 1, (int) toPlace.y] != 1 &&
+                    potentialSlots.Contains(new Vector2(toPlace.x + 1, toPlace.y)) != true) {
+                    potentialSlots.Add(new Vector2(toPlace.x + 1, toPlace.y));
+                }
+                if (toPlace.x > 0 && MiniMap.FeatureMap[(int) toPlace.x - 1, (int) toPlace.y] != 1 &&
+                    potentialSlots.Contains(new Vector2(toPlace.x - 1, toPlace.y)) != true) {
+                    potentialSlots.Add(new Vector2(toPlace.x - 1, toPlace.y));
+                }
+                if (toPlace.y < MiniMap.FeatureMap.GetLength(0) - 1 &&
+                    MiniMap.FeatureMap[(int) toPlace.x, (int) toPlace.y + 1] != 1 &&
+                    potentialSlots.Contains(new Vector2(toPlace.x, toPlace.y + 1)) != true) {
+                    potentialSlots.Add(new Vector2(toPlace.x, toPlace.y + 1));
+                }
+                if (toPlace.y > 0 && MiniMap.FeatureMap[(int) toPlace.x, (int) toPlace.y - 1] != 1 &&
+                    potentialSlots.Contains(new Vector2(toPlace.x, toPlace.y - 1)) != true) {
+                    potentialSlots.Add(new Vector2(toPlace.x, toPlace.y - 1));
                 }
             }
         }
@@ -120,44 +131,70 @@ namespace Assets.Scripts.LevelGeneration {
         /// <param name="enemyWeight">The maximum ratio of enemies for the level</param>
         /// <param name="lootWeight">The maximum ratio of loot for the level</param>
         /// <param name="obstaclesWeight">The maximum ratio of obstacles for the level</param>
-        private void Decorate(int numChunks, float enemyWeight, float lootWeight, float obstaclesWeight){
-            var toDecorate = new List <Vector2>();
-            for(var x=0; x < MiniMap.FeatureMap.GetLength(0); ++x){
-                for(var y = 0; y < MiniMap.FeatureMap.GetLength(1); ++y){
-                    if(MiniMap.FeatureMap[x,y] == 1) { toDecorate.Add(new Vector2(x,y)); } 
-                }
-            }
-
-            var numEnemies = enemyWeight*numChunks;
-            var numLoot = lootWeight*numChunks;
-            var numObstacles = obstaclesWeight*numChunks;
-
-            while(toDecorate.Count > 0){
-                var selectedChunk = toDecorate[Random.Range(0,toDecorate.Count)];
-                toDecorate.Remove(selectedChunk);
-
-                for(var x = 0; x < chunkSize; ++x){
-                    for(var y = 0; y < chunkSize; ++y){
-                        var roll = Random.Range(0f,1f);
-                        if(roll >=.75 && numEnemies >0){
-                                FeatureMap[(int)selectedChunk.x * chunkSize + x, (int)selectedChunk.y * chunkSize + y] =
-                                    (int) LevelDecoration.Enemy;
-                                --numEnemies;
-                        }
-                        else if(roll >=.50 && numObstacles > 0){
-                                FeatureMap[(int)selectedChunk.x * chunkSize + x, (int)selectedChunk.y * chunkSize + y] =
-                                    (int) LevelDecoration.Wall;
-                                --numObstacles;
-                        }
-                        else if(roll >=.25 && numLoot > 0){
-                                FeatureMap[(int)selectedChunk.x * chunkSize + x, (int)selectedChunk.y * chunkSize + y] =
-                                    (int) LevelDecoration.Loot;
-                                --numLoot;
-                        }
+        /// <param name="numChunks">The number of Chunks that where used to create the map</param>
+        private void Decorate(int numChunks, float enemyWeight, float lootWeight, float obstaclesWeight) {
+            // Add each Chunk to the list to decorate
+            var toDecorate = new List<Vector2>();
+            for (var x = 0; x < MiniMap.FeatureMap.GetLength(0); ++x) {
+                for (var y = 0; y < MiniMap.FeatureMap.GetLength(1); ++y) {
+                    // exclude the center chunk that the player will be spawned on
+                    if (MiniMap.FeatureMap[x, y] == 1 && x / 2 != MiniMap.FeatureMap.GetLength(0) &&
+                        y / 2 != MiniMap.FeatureMap.GetLength(1)) {
+                        toDecorate.Add(new Vector2(x, y));
                     }
                 }
             }
 
+            // Calculate the max number of each decoration to use
+            var numEnemies = enemyWeight * numChunks;
+            var numLoot = lootWeight * numChunks;
+            var numObstacles = obstaclesWeight * numChunks;
+
+            // loop till the whole list has been iterated over
+            while (toDecorate.Count > 0) {
+                // grab random chunk and remove it form the list
+                var selectedChunk = toDecorate[Random.Range(0, toDecorate.Count)];
+                toDecorate.Remove(selectedChunk);
+                // decide the number of decorations to place
+                var decorations = Random.Range(0, _chunkSize * _chunkSize / 4);
+
+                // iterate over the chunk and decorate
+                for (var x = 0; x < _chunkSize; ++x) {
+                    for (var y = 0; y < _chunkSize; ++y) {
+                        // see if we have anything left to place
+                        if (decorations > 0) {
+                            // random roll
+                            var roll = Random.Range(0f, 1f);
+                            // drop chance
+                            var chance = 1 - EnemyDropOffset;
+                            // check for drop
+                            if (roll >= chance && numEnemies > 0) {
+                                FeatureMap[(int) selectedChunk.x * _chunkSize + x,
+                                           (int) selectedChunk.y * _chunkSize + y] =
+                                    (int) LevelDecoration.Enemy;
+                                --numEnemies;
+                            }
+                            // update for next check
+                            chance -= ObstacleDropOffset;
+                            // repeat above for each decoration type.
+                            if (roll >= chance && numObstacles > 0) {
+                                FeatureMap[(int) selectedChunk.x * _chunkSize + x,
+                                           (int) selectedChunk.y * _chunkSize + y] =
+                                    (int) LevelDecoration.Wall;
+                                --numObstacles;
+                            }
+                            chance -= LootDropOffset;
+                            if (roll >= chance && numLoot > 0) {
+                                FeatureMap[(int) selectedChunk.x * _chunkSize + x,
+                                           (int) selectedChunk.y * _chunkSize + y] =
+                                    (int) LevelDecoration.Loot;
+                                --numLoot;
+                            }
+                        }
+                        --decorations;
+                    }
+                }
+            }
         }
     }
 }
