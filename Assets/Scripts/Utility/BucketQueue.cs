08393806af45d6fda127d119854046aa59ab3490
Andrew Waugh <Zonr0@users.noreply.github.com>
2017-08-02 15:49:58 -0700

    Adds minor refactors and additions to BucketQueue (#109)
    * Adds an indexer for the BucketQueue
    
    * Modifies BucketQueue remove and updates comments
    
    * Modifies Remove to return a bool. Returns true if found in the given
    bucket, false if otherwise.
    * Adds an overloaded Remove that doesn't require a key
    * Updates various comments throughout the code. Notes where exceptions
    are thrown.
    
    * Removes some includes that were prevent a full Unity build.
    
    * Flips a ternary condition

--- Assets/Scripts/Utility/BucketQueue.cs
+++ Assets/Scripts/Utility/BucketQueue.cs
@@ -2,9 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using NUnit.Framework.Constraints;
 using UnityEngine;
 
 namespace Assets.Scripts.Utility
@@ -89,22 +86,52 @@ namespace Assets.Scripts.Utility
         }
 
         /// <summary>
-        /// Removes a single item from the queue.
+        /// Index operator for the Bucket Queue. Used to read the elements in a particular bucket.
         /// </summary>
-        /// <param name="toRemove"></param>
         /// <param name="key"></param>
-        public void Remove(T toRemove, int key) {
+        /// <returns>An IEnumerable of elements in the particular bucket. Returns an empty enumerable
+        /// if the bucket is empty.</returns>
+        public IEnumerable<T> this[int key] {
+            get {
+                return Buckets.ContainsKey(key) ? Buckets[key] : Enumerable.Empty<T>();
+            } 
+        }
+
+        /// <summary>
+        /// Removes a single item from the queue with a better performance than the general remove.
+        /// </summary>
+        /// <param name="toRemove">The element to remove</param>
+        /// <param name="key">The key that toRemove is associated with</param>
+        /// <returns>False if unable to find the given item to remove in the given bucket. True otherwise.</returns>
+        /// <throws>Throws an ArgumentException if the BucketQueue doesn't have a bucket at the given key</throws>
+        /// <remarks>Only removes the first matching item encountered in the bucket.</remarks>
+        public bool Remove(T toRemove, int key) {
             if (!Buckets.ContainsKey(key) || Buckets[key] == null) {
-                throw new ArgumentException("key");
+                throw new ArgumentException("Bucket specified by key does not exist");
             }
             --Count;
-            Buckets[key].Remove(toRemove);
+            return Buckets[key].Remove(toRemove);
+        }
+
+        /// <summary>
+        /// Removes a single item from the queue without knowing the bucket it's in. Has O(n) worst case performance.
+        /// </summary>
+        /// <param name="toRemove"></param>
+        /// <returns>False if unable to find the given item. True otherwise.</returns>
+        /// <remarks>Only removes the first matching item encountered.</remarks>
+        public bool Remove(T toRemove) {
+            if (Buckets.Values.Any(bucket => bucket != null && bucket.Remove(toRemove))) {
+                --Count;
+                return true;
+            }
+            return false;
         }
 
         /// <summary>
         /// Returns the first item in the queue
         /// </summary>
         /// <returns>The item at the front of the queue</returns>
+        /// <throws>Throws an InvalidOperationException if the queue is empty</throws>
         public T Peek() {
             if (Count == 0) {
                 throw new InvalidOperationException("Empty Queue");}
