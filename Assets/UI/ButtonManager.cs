b6177ad16d61f05ca2a27d75edb36823e65a48b5
mgieger <mrgieger@gmail.com>
2017-08-06 15:55:34 -0700

    Basic menu Added. (#119)

---
+++ Assets/UI/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void StartGameBtn(string StartGame) {
+        SceneManager.LoadScene(StartGame);
+    }
+
+    public void ExitGameBtn() {
+        Application.Quit();
+    }
+
+}
