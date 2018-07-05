8c35c3f9d35ddd9f230eb2b83e5f34961f191857
TsebMagi <Doug.b.Whitley@gmail.com>
2018-07-04 18:38:27 -0700

    updates to combat namespace to reduce linkage

--- Assets/Scripts/Combat/MeleeAttack.cs
+++ Assets/Scripts/Combat/MeleeAttack.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 using System;
 //TODO: Design and Implement class
 namespace Combat{
+    public enum MeleeBehaviour{
+        dash,
+        step,
+        
+    }
     public class MeleeAttack : AttackStats {
         MeleeAttack(AttackType Type, int Damage, DamageType[] DamageTypes, float ForceToApply, string Description):base(Type,Damage,DamageTypes,ForceToApply,Description){
 
